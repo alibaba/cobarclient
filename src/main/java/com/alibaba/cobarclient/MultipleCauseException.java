7e75f4791f835f4bb5cdd2273753b656053049ce
fujohnwang <fujohnwang@gmail.com>
2013-07-28 15:38:51 +0800

    add BestEffortMultiDataSourceTransactionManager impl.

--- src/main/java/com/alibaba/cobarclient/MultipleCauseException.java
+++ src/main/java/com/alibaba/cobarclient/MultipleCauseException.java
@@ -3,15 +3,10 @@ package com.alibaba.cobarclient;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created with IntelliJ IDEA.
- * User: fujohnwang
- * Date: 7/28/13
- * Time: 2:59 PM
- * To change this template use File | Settings | File Templates.
- */
 public class MultipleCauseException extends Throwable {
     private List<Throwable> causes = new ArrayList<Throwable>();
+
+    public MultipleCauseException(){}
 
     public MultipleCauseException(List<Throwable> causes) {
         if (!(causes == null || causes.isEmpty())) this.causes.addAll(causes);
--- src/main/java/com/alibaba/cobarclient/transaction/BestEffortMultiDataSourceTransactionManager.java
+++ src/main/java/com/alibaba/cobarclient/transaction/BestEffortMultiDataSourceTransactionManager.java
@@ -1,16 +1,25 @@
 package com.alibaba.cobarclient.transaction;
 
+import com.alibaba.cobarclient.MultipleCauseException;
 import com.alibaba.cobarclient.Shard;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.TransactionException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.*;
 import org.springframework.transaction.support.AbstractPlatformTransactionManager;
 import org.springframework.transaction.support.DefaultTransactionStatus;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
-public class BestEffortMultiDataSourceTransactionManager extends AbstractPlatformTransactionManager {
+public class BestEffortMultiDataSourceTransactionManager extends AbstractPlatformTransactionManager implements InitializingBean {
 
     private Set<Shard> shards;
+
+    protected List<AbstractPlatformTransactionManager> transactionManagers;
+
+    public BestEffortMultiDataSourceTransactionManager() {
+    }
 
     public BestEffortMultiDataSourceTransactionManager(Set<Shard> shards) {
         this.shards = shards;
@@ -18,22 +27,50 @@ public class BestEffortMultiDataSourceTransactionManager extends AbstractPlatfor
 
     @Override
     protected Object doGetTransaction() throws TransactionException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return new ArrayList<DefaultTransactionStatus>();
     }
 
     @Override
     protected void doBegin(Object o, TransactionDefinition transactionDefinition) throws TransactionException {
-        //To change body of implemented methods use File | Settings | File Templates.
+        List<TransactionStatus> statusList = (List<TransactionStatus>) o;
+        for (AbstractPlatformTransactionManager transactionManager : transactionManagers) {
+            statusList.add(transactionManager.getTransaction(transactionDefinition));
+        }
     }
 
     @Override
     protected void doCommit(DefaultTransactionStatus defaultTransactionStatus) throws TransactionException {
-        //To change body of implemented methods use File | Settings | File Templates.
+        MultipleCauseException ex = new MultipleCauseException();
+        List<TransactionStatus> statusList = (List<TransactionStatus>) defaultTransactionStatus.getTransaction();
+        for (int i = transactionManagers.size() - 1; i >= 0; i--) {
+            AbstractPlatformTransactionManager transactionManager = transactionManagers.get(i);
+            TransactionStatus status = statusList.get(i);
+            try {
+                transactionManager.commit(status);
+            } catch (TransactionException e) {
+                ex.add(e);
+            }
+        }
+        if (!ex.getCauses().isEmpty())
+            throw new HeuristicCompletionException(HeuristicCompletionException.STATE_UNKNOWN, ex);
+
     }
 
     @Override
     protected void doRollback(DefaultTransactionStatus defaultTransactionStatus) throws TransactionException {
-        //To change body of implemented methods use File | Settings | File Templates.
+        MultipleCauseException ex = new MultipleCauseException();
+        List<TransactionStatus> statusList = (List<TransactionStatus>) defaultTransactionStatus.getTransaction();
+        for (int i = transactionManagers.size() - 1; i >= 0; i--) {
+            AbstractPlatformTransactionManager transactionManager = transactionManagers.get(i);
+            TransactionStatus status = statusList.get(i);
+            try {
+                transactionManager.rollback(status);
+            } catch (TransactionException e) {
+                ex.add(e);
+            }
+        }
+        if (!ex.getCauses().isEmpty())
+            throw new UnexpectedRollbackException("one or more error on rolling back the transaction", ex);
     }
 
     public Set<Shard> getShards() {
@@ -44,4 +81,14 @@ public class BestEffortMultiDataSourceTransactionManager extends AbstractPlatfor
         this.shards = shards;
     }
 
+    public void afterPropertiesSet() throws Exception {
+        if (shards == null || shards.isEmpty()) throw new IllegalArgumentException("'shards' is required.");
+        transactionManagers = new ArrayList<AbstractPlatformTransactionManager>();
+        for (Shard shard : shards) {
+            DataSourceTransactionManager transactionManager = new DataSourceTransactionManager(shard.getDataSource());
+            transactionManager.setDefaultTimeout(getDefaultTimeout());
+            transactionManager.setTransactionSynchronization(getTransactionSynchronization());
+            transactionManagers.add(transactionManager);
+        }
+    }
 }
